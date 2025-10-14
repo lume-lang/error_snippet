5655af10c7ee2dac92dbc45fa872c5ebe5c592d1
Max T. Kristiansen <me@maxtrier.dk>
2025-10-14 12:45:04 +0200

    test: update test snapshot files

--- tests/tests.rs
+++ tests/tests.rs
@@ -7,5 +7,6 @@ fn render(diagnostic: impl Diagnostic) -> String {
     let mut renderer = GraphicalRenderer::new();
     renderer.use_colors = false;
 
-    owo_colors::with_override(false, || renderer.render(&diagnostic).unwrap().to_string())
+    owo_colors::set_override(false);
+    renderer.render(&diagnostic).unwrap().to_string()
 }
